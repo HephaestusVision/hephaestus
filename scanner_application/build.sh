b7575d3733489b5397c0f054ad52ba72995b9eec
Hal Canary <hal@cs.unc.edu>
2012-11-02 13:03:30 -0400

    updated build system, added PCL library; we will need it later.

--- scanner_application/build.sh
+++ scanner_application/build.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-OpenCV_DIR="/home/hal/build/OpenCV"
-VTK_DIR="/home/hal/build/VTK-5-10"
-LIB_FREENECT_SRC="/home/hal/src/libfreenect"
-LIB_FREENECT_BUILD="/home/hal/build/libfreenect"
-
-export VTK_DIR
-export OpenCV_DIR
-export LIB_FREENECT_SRC
-export LIB_FREENECT_BUILD
+## Example build script for Linux.  Change the paths to appropriate
+## locations.  Or just use cmake-gui.
 
 SRC=$(cd $(dirname "$0"); pwd)
 BUILD="${SRC}/build"
 mkdir -p "$BUILD"
 cd "$BUILD"
-cmake "$SRC" && make && echo "SUCCESS!";
+cmake "$SRC" \
+    -DVTK_DIR="${HOME}/build/VTK-5-10" \
+    -DOpenCV_DIR:PATH="${HOME}/build/OpenCV" \
+    -DLIB_FREENECT_SRC:PATH="${HOME}/src/libfreenect" \
+    -DLIB_FREENECT_BUILD:PATH="${HOME}/build/libfreenect" \
+    && make && echo "SUCCESS!";
+
